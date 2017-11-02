a1aaa76b6e9efb72fc9fb89b89aee0bc9d427255
Alex <alex.cachnd@gmail.com>
2017-11-02 14:46:16 -0400

    Eliminar Texto innecesario

--- Funciones.java
+++ Funciones.java
@@ -1,14 +1,7 @@
 public class Funciones{
-<<<<<<< HEAD
-	public int resta(int a, int b){
-		return a-b;
-	} 
 
 	public int suma(int a, int b){
 		return a+b;
 	}
-=======
 
-
->>>>>>> parent of e4c78f4... funcion de resta agregada
 }
